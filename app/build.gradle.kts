b83fd6669db1d2270e15594a7d1ab0732e0191a6
ppcorn <junfhu@outlook.com>
2026-02-01 21:45:57 +0800

    upgrade to 0.11, replace the tick_weak.mp3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.heyu.metronome"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
